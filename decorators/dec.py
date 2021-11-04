d911e519598e84972921c3b9bed786cad08e99dc
Aslan <asl.zurabov@gmail.com>
2021-11-04 14:12:28 +0300

    decorator with unknown number of params

--- decorators/dec.py
+++ decorators/dec.py
@@ -30,7 +30,7 @@ NamePrint()
 
 def numcheck(func):
     def checkInt(d):
-        if isinstance(d,int):
+        if isinstance(d, int):
             if d == 0:
                 print('Can\'t divide by zero')
                 return False
@@ -38,17 +38,54 @@ def numcheck(func):
         print(f'{d} in not a number')
         return False
 
-
     def inner(x, y):
         if not checkInt(x) or not checkInt(y):
             return
-        return func(x,y)
+        return func(x, y)
+
     return inner
+
 
 @numcheck
 def divide(a, b):
     print(a / b)
 
 
+# Decorator with unknown number fo params
+# We want a decorator that can pass params and handle anything
+# we also want to chain them together
+# *args, **kwargs to the rescue
+
+def outline(func):
+    def inner(*args, **kwargs):
+        print('~' * 20)
+        func(*args, **kwargs)
+        print('~' * 20)
+
+    return inner
 
 
+def list_items(func):
+    def inner(*args,**kwargs):
+        func(*args,**kwargs)
+        # print(f'args = {args}')
+        # print(f'kwargs = {kwargs}')
+        for i in args:
+            print(f'args = {i}')
+        for k,v in kwargs.items():
+            print(f'key={k} value={v}')
+    return inner
+
+
+@outline
+@list_items
+def display(msg):
+    print(msg)
+
+display(msg=433)
+
+
+a = {'as':342,'dd':34}
+print(a.items())
+print(a.values())
+print(a.keys())
