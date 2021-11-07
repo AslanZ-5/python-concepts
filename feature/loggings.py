4d8ca9aaae1b7f959416c81a03c3d4e4ce20a00d
Aslan <asl.zurabov@gmail.com>
2021-11-07 11:17:14 +0300

    logging to a file

--- feature/loggings.py
+++ feature/loggings.py
@@ -13,5 +13,20 @@ def test():
 
 
 rootlog = logging.getLogger()
+print('level : ' + logging.getLevelName(rootlog.getEffectiveLevel()))
 rootlog.setLevel(logging.INFO) # setting new level of logging
+test()
+
+
+# log to file
+# basicConfig only works if the logger has not been configured before
+# logging.basicConfig(filename='app.txt',filemode='w',format='%(levelname)s:%(message)s',level=logging.DEBUG)
+# logging.debug('hellooooo')
+
+handler = logging.FileHandler('file.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+rootlog.addHandler(handler)
+rootlog.setLevel(logging.DEBUG)
+rootlog.debug('tessssttt')
 test()
