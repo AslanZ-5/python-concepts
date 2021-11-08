c696eec3395bc69aac4a70b994270f1e1afe83de
Aslan <asl.zurabov@gmail.com>
2021-11-08 10:44:36 +0300

    OOP getitem

--- OOP/getsetitem.py
+++ OOP/getsetitem.py
@@ -2,17 +2,21 @@ class Indexer:
     data = [5, 6, 7, 8, 9]
 
     def __getitem__(self, item):
-        if isinstance(item,int) and item <= len(self.data)-1:
+        if isinstance(item, int) and item <= len(self.data) - 1:
             return self.data[item]
-        elif item.stop <= len(self.data)-1:
+        elif item.stop <= len(self.data) - 1:
             return self.data[item]
         else:
             return 'some error'
 
 
+class StepperIndex:
+    def __getitem__(self, item):
+        return self.data[item]
 
 
+x = StepperIndex()
+x.data = 'spam'
 
-a = Indexer()
-
-print(a[1:4])
+for i in x:
+    print(i, end=' ')
