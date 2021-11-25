5301997bce942d1306045a569cf2ee9e19c2c9d0
Aslan <asl.zurabov@gmail.com>
2021-11-25 08:34:13 +0300

    __add__

---
+++ OOP/add_and_r_iadd.py
@@ -0,0 +1,38 @@
+class Commuter2:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    def __radd__(self, other):
+        return self.__add__(other)  # Call __add__ explicity
+
+
+class Commuter3:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    def __radd__(self, other):
+        return self + other
+
+
+class Commuter4:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+
+    __radd__ = __add__
+
+
+a = Commuter4git(43)
+print(a + 10)
+print(a.val)
