9efcfa38a1446de766099c13af87b67e28eb3c3c
Aslan <asl.zurabov@gmail.com>
2021-12-18 08:58:33 +0300

    self less methods

--- OOP/methods_are_objects.py
+++ OOP/methods_are_objects.py
@@ -19,3 +19,20 @@ class Eggs:
 if __name__ == '__main__':
     d = Eggs()
     d.m2()
+
+
+
+class Selfless:
+    def __init__(self,data):
+        self.data = data
+    def selfless(arg1,arg2):
+        return arg1 + arg2
+    def normal(self,arg1,arg2):
+        return self.data + arg1 + arg2
+
+if __name__ == '__main__':
+    print('++++++++++++++')
+    X = Selfless(2)
+    print(X.normal(3,4))
+    print(Selfless.normal(X,4,5))
+    print(Selfless.selfless(4,5))
