29a2d3af648d7cf4880f4dc32e701d9a7f2f38b3
Aslan <asl.zurabov@gmail.com>
2022-01-03 10:07:04 +0300

    OOP Method Resolution Order (MRO)

--- OOP/mro.py
+++ OOP/mro.py
@@ -1,24 +1,18 @@
 class A:
-    num = 10
+    attr = 1
+
+
 class B(A):
     pass
+
+
 class C(A):
-    num = 1
+    attr = 2
+
+
 class D(B, C):
-    pass
+    attr = B.attr 
 
 
-class Y:
-    pass
-class Z:
-    pass
-class X:
-    pass
-class B(Y,Z):
-    pass
-class A(X,Y):
-    pass
-class M(B,A,Z):
-    pass
-
-print(M.mro())
+x = D()
+print(x.attr)
