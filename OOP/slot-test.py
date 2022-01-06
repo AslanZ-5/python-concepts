423344aa08b84b4e79b1fcd4cf1cf4151866329e
Aslan <asl.zurabov@gmail.com>
2022-01-06 10:09:10 +0300

    testing slots through comparing running time between slot class and Noneslote class

---
+++ OOP/slot-test.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import timeit
+
+base = """
+Is = [] 
+for i in range(1000):
+    X = C()
+    X.a = 1; X.b = 2; X.c =3 ; X.d=4
+    t = X.a + X.b + X.c + X.d
+    Is.append(X)
+"""
+stmt = """
+class C:
+    __slots__ = ['a','b','c','d']
+""" + base
+print('Slots  =>',end=' ')
+print(min(timeit.repeat(stmt,number=1000,repeat=3)))
+
+stmt = """
+class C:
+    pass
+""" + base
+print('NoneSlots  =>',end=' ')
+print(min(timeit.repeat(stmt,number=1000,repeat=3)))
--- OOP/slots_oop.py
+++ OOP/slots_oop.py
@@ -18,9 +18,9 @@ class D(T):
     __slots__ = ['a']
 
 
-X = D()
-X.a = 1
-X.b = 2
+# X = D()
+# X.a = 1
+# X.b = 2
 # print(X.__dict__)
 # print(D.__dict__.keys())
 
@@ -30,11 +30,13 @@ class Y:
 class R(Y):
     pass
 
-I  = R()
-I.a = 1
-I.b = 2
-print(I.__dict__)
-print(Y.__dict__.keys())
+# I  = R()
+# I.a = 1
+# I.b = 2
+# print(I.__dict__)
+# print(Y.__dict__.keys())
 # only lowerst slot accssible
 class W: __slots__ = ['a']
 class U(W): __slots__ = ['a']
+
+
