53f42255cb72f096b251d8e6b3e48d844d6a4ba8
Aslan <asl.zurabov@gmail.com>
2022-01-16 14:41:51 +0300

    super()

--- OOP/the_super.py
+++ OOP/the_super.py
@@ -1,16 +1,26 @@
 class A:
+    def __init__(self):
+        print('init A')
     def act(self):
         print('A')
 
 
 class B:
+    def __init__(self):
+        super().__init__()
+        print('init b')
     def act(self):
         print('B')
 
 
-class C(A, B):
+class C(A,B):
+    def __init__(self):
+        super().__init__()
     def act(self):
-        B.act(self)
+        super().act()
 
-x = C()
-x.act()
+
+
+a = A()
+b = B()
+c = C()
