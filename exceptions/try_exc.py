07e0092d4596cfa7f3aedc3d8d2919d1cdb57166
Aslan <asl.zurabov@gmail.com>
2022-01-23 14:08:20 +0300

    exptions (try except finally else)

--- exceptions/try_exc.py
+++ exceptions/try_exc.py
@@ -1,11 +1,23 @@
-def inx_item(item,inx):
-    return item[inx]
+# def inx_item(item,inx):
+#     return item[inx]
+#
+#
+# try:
+#     inx_item([1,2,3],4)
+# except IndexError as a:
+#     print(f'got error -- {a}')
+#
+# finally:
+#     print('The end')
 
-
+b = [1]
 try:
-    inx_item([1,2,3],4)
-except IndexError as a:
-    print(f'got error -- {a}')
+    print(b[32])
+except ZeroDivisionError:
+    print('dddi')
+else:
+    print('else')
 
 finally:
-    print('The end')
+    print('get error')
+print('print it ')
