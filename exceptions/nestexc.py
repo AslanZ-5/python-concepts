e382da475533dd98b388c759fafc1accf0897708
Aslan <asl.zurabov@gmail.com>
2022-01-30 10:40:43 +0300

    exceptions try/finally

--- exceptions/nestexc.py
+++ exceptions/nestexc.py
@@ -1,5 +1,7 @@
 def action2():
     print(1 + [])
+
+
 def action1():
     try:
         action2()
@@ -11,3 +13,57 @@ try:
     action1()
 except TypeError:
     print('outer try')
+
+try:
+    try:
+        action2()
+    except TypeError:
+        print('---inner')
+except TypeError:
+    print('--- outer')
+
+
+# # Try/Finally
+# try:
+#     try:
+#         raise IndexError
+#     finally:
+#         print('span')
+# finally:
+#     print('smap')
+
+
+def raise1():
+    raise IndexError
+
+
+def noraise():
+    return
+
+
+def raise2():
+    raise SyntaxError
+
+# for func in (raise1,noraise,raise2):
+#     print('<%s>' % func.__name__)
+#     try:
+#         try:
+#             func()
+#         except IndexError:
+#             print('caught IndexError')
+#     finally:
+#         print('Finnally run ')
+#     print('.........')
+
+class ExitLoop(Exception):pass
+try:
+    while True:
+        while True:
+            for i in range(10):
+                if i > 3 :
+                    raise ExitLoop
+                print(f'loop3: {i}')
+            print('loop2')
+        print('loop1')
+except ExitLoop:
+    print('continuing')
