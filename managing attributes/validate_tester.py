d1a6a91139695961a3af2e95efd752c957ce9a5a
мансур зурабов <asl.zurabov@gmail.com>
2022-02-09 11:20:06 +0300

    Using Properties to Validate OOP

---
+++ managing attributes/validate_tester.py
@@ -0,0 +1,9 @@
+from validate_property import CardHolder
+
+bob = CardHolder('1234-3216','Bob Smith',40,'123 main st')
+print(bob)
+print('\n')
+bob.name = 'Bob Q. Smith'
+bob.age = 50
+bob.acct = '23-45-67-89'
+print(bob)
